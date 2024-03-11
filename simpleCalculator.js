5d08e090c300385368262a1e03e0187605b837da
User <ndnx300@gmail.com>
2024-03-11 11:42:57 +0000

    app more features

--- simpleCalculator.js
+++ simpleCalculator.js
@@ -16,3 +16,37 @@ catch(error){
   display.value = "Error";
 }
 }
+
+// Add event listener to capture keypress events
+document.addEventListener("keypress", function(event) {
+  // Check which key is pressed and call corresponding function
+  switch(event.key) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      appendToDisplay(event.key);
+      break;
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+    case ".":
+      appendToDisplay(event.key);
+      break;
+    case "Enter":
+      calculate();
+      break;
+    case "c":
+      clearDisplay();
+      break;
+    default:
+      break;
+  }
+});
